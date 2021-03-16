0bd83b4821178d72c55dd52caeec94264ce13cf7
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-03-16 19:55:02 -0300

    finished addind a popup validate lib and data checker

--- fundamentals_of_web_development/chapter_06/day_02/script.js
+++ fundamentals_of_web_development/chapter_06/day_02/script.js
@@ -30,21 +30,9 @@ const stateArray = [
 ];
 
 // Elements
-const myForm = document.querySelector("#myForm");
 const selectState = document.querySelector("#states");
-const dateInput = document.querySelector("#start_date");
-const sendBtn = document.querySelector("#submitBtn");
-const successMessage = document.querySelector("#sentMessage");
-const allInputs = Array.from(document.querySelectorAll("input"));
-allInputs.push(document.querySelector("textarea"));
+const picker = new Pikaday({ field: document.getElementById("startDate") });
 // Functions
-
-const insertError = (text) => {
-  const errorP = document.createElement("p");
-  errorP.classList.add("error");
-  errorP.innerText = text;
-  return errorP;
-};
 
 const loadStates = () => {
   for (let state of stateArray) {
@@ -55,107 +43,9 @@ const loadStates = () => {
   }
 };
 
-const validateDate = (date) => {
-  if (date.indexOf("/") === 2 || date.indexOf("/") === 5) {
-    const splitDate = date.split("/");
-    const day = splitDate[0];
-    const month = splitDate[1];
-    const year = splitDate[2];
-    if (day > 0 && day <= 31 && month > 0 && month <= 12 && year > 0 && year.length === 4) {
-      return true;
-    }
-    return false;
-  }
-};
-
-const removeElements = (elements) => {
-  elements.forEach((element) => {
-    element.parentNode.removeChild(element);
-  });
-};
-
-//  Checks
-
-const checkDate = () => {
-  const inputDate = document.querySelector("#start_date");
-  const date = inputDate.value;
-  if (!date) return;
-  let hasError = false;
-  const validDate = validateDate(date);
-  if (!validDate) {
-    const errorMessage = "Invalid Format Date";
-    inputDate.parentNode.parentNode.insertBefore(insertError(errorMessage), inputDate.parentNode);
-    hasError = true;
-  }
-  return hasError;
-};
-
-const checkEmail = () => {
-  const email = document.querySelector("#email");
-  if (!email.value) {
-    return;
-  }
-  const re = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/;
-  if (!re.test(email.value) && email.value) {
-    const errorMessage = "Invalid Email";
-    email.parentNode.parentNode.insertBefore(insertError(errorMessage), email.parentNode);
-  } else {
-    return true;
-  }
-};
-
-const checkEmptyInputs = () => {
-  let hasError = false;
-  allInputs.forEach((input) => {
-    if (input.value === "" && input.type != "radio") {
-      const error = `${input.parentNode.innerText.match(/[^:]+/)} must be filled out`;
-      hasError = true;
-      input.parentNode.parentNode.insertBefore(insertError(error), input.parentNode);
-    }
-  });
-  return hasError;
-};
-
-const checkMaxLength = () => {
-  let hasError = false;
-  allInputs.forEach((input) => {
-    if (input.value > input.maxLength && input.maxLength != -1) {
-      const error = `${input.parentNode.innerText.match(/[^:]+/)} has more than ${
-        input.maxLength
-      } characters`;
-      hasError = true;
-      input.parentNode.parentNode.insertBefore(insertError(error), input.parentNode);
-    }
-  });
-  return hasError;
-};
-
 //             Listeners
-
-const setSendBtn = () => {
-  sendBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    const elementErrors = document.querySelectorAll(".error");
-    if (elementErrors) {
-      removeElements(elementErrors);
-    }
-    const invalidLength = checkMaxLength();
-    const hasEmptyInputs = checkEmptyInputs();
-    const invalidEmail = checkEmail();
-    const invalidDate = checkDate();
-    if (invalidLength || invalidEmail || invalidDate || hasEmptyInputs) {
-      successMessage.innerText = "Something went Wrong, check the form";
-    } else {
-      successMessage.innerText = "Sendind Data";
-    }
-  });
-};
-
-const loadButtons = () => {
-  setSendBtn();
-};
 
 window.onload = () => {
   loadStates();
-  loadButtons();
+  validation.init("#myForm");
 };
