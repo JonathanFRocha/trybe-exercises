5019b95f54b5c06d07685957ccfa995cd77cd7d3
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-04-13 20:08:59 -0300

    finished second part using promises

--- fundamentals_of_web_development/chapter_09/day_02/jokes.js
+++ fundamentals_of_web_development/chapter_09/day_02/jokes.js
@@ -9,7 +9,47 @@ const fetchJoke = () => {
 
   fetch(API_URL, header)
     .then((resp) => resp.json())
-    .then((json) => console.log(json.joke));
+    .then((json) => {
+      const element = document.querySelector("#jokeContainer");
+      element.innerHTML = json.joke;
+    });
 };
+
+const getArray = (arr) => {
+  const eArr = [];
+  for (let i = 0; i < arr; i += 1) {
+    const rNumber = Math.ceil(Math.random() * 50);
+    eArr.push(rNumber * rNumber);
+  }
+  return eArr;
+};
+
+const promise = new Promise((res, rej) => {
+  const tenArray = getArray(10);
+  const totalOfTenArray = tenArray.reduce((acc, item) => acc + item);
+  totalOfTenArray < 8000 ? res(totalOfTenArray) : rej();
+})
+  .then((res) => {
+    const divisors = [2, 3, 5, 10];
+    return divisors.map((item, index) => {
+      switch (index) {
+        case 0:
+          return res / divisors[index];
+
+        case 1:
+          return res / divisors[index];
+
+        case 2:
+          return res / divisors[index];
+
+        case 3:
+          return res / divisors[index];
+      }
+    });
+  })
+  .then((arr) => console.log(arr.reduce((acc, item) => acc + item)))
+  .catch((err) => console.log("É mais de oito mil! Essa promise deve estar quebrada!"));
+
+console.log(promise);
 
 window.onload = () => fetchJoke();
