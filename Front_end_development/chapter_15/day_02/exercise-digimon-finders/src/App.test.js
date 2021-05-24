f80ab9b2173d1a9135e5af658ba7282095c7f758
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-05-24 20:59:55 -0300

    finished api test

--- Front_end_development/chapter_15/day_02/exercise-digimon-finders/src/App.test.js
+++ Front_end_development/chapter_15/day_02/exercise-digimon-finders/src/App.test.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { fireEvent, render } from "@testing-library/react";
+import { findByText, fireEvent, render } from "@testing-library/react";
 import App from "./App";
 
 describe("Test all", () => {
+  afterEach(() => jest.clearAllMocks());
   it("renders App", async () => {
     const { getByText, getByTestId } = render(<App />);
 
@@ -30,5 +31,38 @@ describe("Test all", () => {
     const digimon = queryByTestId("digimonName");
 
     expect(digimon).toBe(null);
+  });
+
+  test("Search by digimon", async () => {
+    const digimon = [
+      {
+        name: "Patamon",
+        img: "https://digimon.shadowsmith.com/img/patamon.jpg",
+        level: "Rookie",
+      },
+    ];
+    jest.spyOn(global, "fetch");
+    const globalFetch = global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(digimon),
+    });
+
+    const { findByText, getByTestId, getByAltText } = render(<App />);
+
+    const input = getByTestId("input");
+    expect(input).toHaveValue("");
+
+    fireEvent.change(input, { target: { value: "Patamon" } });
+    expect(input).toHaveValue("Patamon");
+
+    const button = getByTestId("buttonSearch");
+    expect(button).toBeInTheDocument();
+    fireEvent.click(button);
+
+    await findByText("Patamon");
+    expect(getByTestId("digimonLevel")).toBeInTheDocument();
+    expect(getByAltText("Patamon")).toBeInTheDocument();
+
+    expect(globalFetch).toBeCalledTimes(1);
+    expect(globalFetch).toBeCalledWith("https://digimon-api.vercel.app/api/digimon/name/Patamon");
   });
 });
