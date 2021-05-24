7f78ca667c8a8b61431891c3e0c1566f7abccf6b
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-05-24 20:31:53 -0300

    two tests done on APP to see if input is working

--- Front_end_development/chapter_15/day_02/exercise-digimon-finders/src/App.test.js
+++ Front_end_development/chapter_15/day_02/exercise-digimon-finders/src/App.test.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { fireEvent, render } from "@testing-library/react";
+import App from "./App";
 
-describe('Teste da aplicação toda', () => {
+describe("Test all", () => {
+  it("renders App", async () => {
+    const { getByText, getByTestId } = render(<App />);
 
-  it('renders App', async () => {
-    const { getByText } = render(<App />);
     const linkElement = getByText(/Faça uma pesquisa/i);
+    const makeSearch = getByText(/Search Digimon/i);
+
+    expect(makeSearch).toBeInTheDocument();
     expect(linkElement).toBeInTheDocument();
+
+    const searchInput = getByTestId("input");
+    expect(searchInput).toHaveTextContent("");
   });
 
+  it("has value on the inputbox", () => {
+    const { getByTestId } = render(<App />);
+    const input = getByTestId("input");
+    expect(input).toHaveValue("");
+
+    fireEvent.change(input, { target: { value: "Agumon" } });
+    expect(input).toHaveValue("Agumon");
+  });
 });
