d760d572130cce3c6fc85494aa2026eb1a454841
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-05-24 22:11:45 -0300

    testing with react router

---
+++ Front_end_development/chapter_15/day_03/firstpart/src/App.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import renderWithRouter from "./renderWithRouter";
+import App from "./App";
+import { fireEvent } from "@testing-library/react";
+describe("teste da aplicação toda", () => {
+  test("deve renderizar o componente App", () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText(/Você está na página Início/);
+    expect(home).toBeInTheDocument();
+  });
+
+  test("deve renderizar o componente Sobre", () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Sobre/i));
+    const pathname = history.location.pathname;
+    expect(pathname).toBe("/about");
+    const aboutAll = getByText(/Você está na página Sobre/);
+    expect(aboutAll).toBeInTheDocument();
+  });
+});
