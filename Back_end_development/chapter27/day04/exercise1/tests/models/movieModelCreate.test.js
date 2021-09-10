9fa1495c0fa38c2a7d6ea8f27ec9881a4acd5192
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-09-09 23:14:10 -0300

    finished

--- Back_end_development/chapter27/day04/exercise1/tests/models/movieModelCreate.test.js
+++ Back_end_development/chapter27/day04/exercise1/tests/models/movieModelCreate.test.js
@@ -1,6 +1,9 @@
 const { expect } = require("chai");
 const sinon = require("sinon");
 
+const { MongoClient } = require("mongodb");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+
 const mongoConnection = require("../../models/connection");
 const MoviesModel = require("../../models/movieModel");
 
@@ -13,20 +16,16 @@ describe("Insere um novo filme no BD", () => {
     releaseYear: 1999,
   };
 
-  before(() => {
-    const ID_EXAMPLE = "604cb554311d68f491ba5781";
-    const insertOne = async () => ({ insertedId: ID_EXAMPLE });
-    const collection = async () => ({ insertOne });
-    const db = async (databaseName) => ({ collection });
-    const getConnectionMock = async () => ({ db });
+  before(async () => {
+    const DBServer = new MongoMemoryServer();
+    const URLMock = await DBServer.getUri();
 
-    connectionMock = getConnectionMock().then((conn) => conn.db("model_example"));
+    connectionMock = await MongoClient.connect(URLMock, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then((conn) => conn.db("model_example"));
 
     sinon.stub(mongoConnection, "getConnection").resolves(connectionMock);
-  });
-
-  after(() => {
-    mongoConnection.getConnection.restore();
   });
 
   describe("quando é inserido com sucesso", () => {
@@ -40,6 +39,14 @@ describe("Insere um novo filme no BD", () => {
       const response = await MoviesModel.create(payloadMovie);
 
       expect(response).to.have.a.property("id");
+    });
+
+    it("deve existir um filme com o título cadastrado!", async () => {
+      await MoviesModel.create(payloadMovie);
+      const movieCreated = await connectionMock
+        .collection("movies")
+        .findOne({ title: payloadMovie.title });
+      expect(movieCreated).to.be.not.null;
     });
   });
 });
