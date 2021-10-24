85cdd4d6a6c605ec84f20e27fa8c7a8179115861
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-10-23 22:01:16 -0300

    first real time chat finished

--- Back_end_development/chapter31/day03/socketExercise/index.js
+++ Back_end_development/chapter31/day03/socketExercise/index.js
@@ -1,11 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').createServer(app);
+const http = require("http").createServer(app);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
+require("./sockets/ping")(io);
+require("./sockets/chat")(io);
+require("./sockets/rooms")(io);
+
+app.use(express.static(__dirname + "/public"));
+
 http.listen(3000, () => {
-  console.log('Servidor ouvindo na porta 3000');
+  console.log("Servidor ouvindo na porta 3000");
 });
